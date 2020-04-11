c9ed99c06c6b9c5a4d3aad54c473bc7e2dd21915
OpenCLoud <alejandro.torres@opencloud.es>
2020-04-11 21:10:56 +0200

    Ajustes para deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -25,6 +25,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 // arrancar la app
-app.listen(PORT, () => {
-  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port,'0.0.0.0', () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
