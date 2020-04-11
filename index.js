762ce715b41268371f4d299463adedeaaee38829
OpenCLoud <alejandro.torres@opencloud.es>
2020-04-11 21:49:39 +0200

    Cgange cors

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 conectarDB();
 
 // hablitar cors
-// app.use(cors({ credentials: true, origin: true }));
-// app.options("*", cors());
+app.use(cors({ credentials: true, origin: true }));
+app.options("*", cors());
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "domain"); // update to match the domain you will make the request from
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,13 +18,7 @@ conectarDB();
 //   next();
 // });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
 // Habilitar express.json
 app.use(express.json({ extended: true }));
 
